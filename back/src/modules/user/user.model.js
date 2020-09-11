bf06d3cb96ec072aa159997eb8cb507f2b9ff97b
Dorian Latouche <d.latouche@betterfly-tourism.com>
2020-09-11 15:18:02 +0200

    Create user model

--- back/src/modules/user/user.model.js
+++ back/src/modules/user/user.model.js
@@ -1,6 +1,10 @@
 export default class User {
-    constructor(id, name){
-        this.id = id;
-        this.name = name;
+
+    constructor(user) {
+        const { id, name, password, role } = user
+        this.id = id
+        this.name = name
+        this.password = password
+        this.role = role
     }
 }
--- back/src/modules/user/user.test.js
+++ back/src/modules/user/user.test.js
@@ -1,16 +1,24 @@
 import User from './user.model';
 
 describe('User model testing', () => {
+    let user = null
+    beforeEach(() => {
+        user = new User({ id: 0, name: "Zwerque", password: "Rien-0707", role: 1 })
+    })
+
     it('user model exist', async () => {
-        let user = new User()
         expect(user instanceof User).toBe(true);
     })
     it('id should be number', async () => {
-        let user = new User(1)
         expect(typeof user.id).toBe("number");
     })
     it('name should be string', async () => {
-        let user = new User(1, "name")
         expect(typeof user.name).toBe("string");
+    })
+    it('password should be string', async () => {
+        expect(typeof user.password).toBe("string");
+    })
+    it('role should be string', async () => {
+        expect(typeof user.role).toBe("number");
     })
 });
